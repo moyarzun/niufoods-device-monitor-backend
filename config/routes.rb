3295ae74063ff3efda47f87525a8b339b931222a
moyarzun <moyarzun@buk.cl>
2024-09-09 11:03:20 -0300

    fix: restriccion de endpoints disponibles para API

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :status_logs
+      resources :status_logs, only: %i[index show create]
     end
   end
   namespace :api do
     namespace :v1 do
-      resources :status
+      resources :status, only: %i[index show]
     end
   end
   namespace :api do
     namespace :v1 do
-      resources :devices
+      resources :devices, only: %i[index show create update]
     end
   end
   namespace :api do
     namespace :v1 do
-      resources :locations
+      resources :locations, only: %i[index show create update]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
